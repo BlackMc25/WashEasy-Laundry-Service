14a3062872a958117e5170ce40928ec5bdcbac25
BlackMc25 <christonwodika@gmail.com>
2026-08-12 18:24:45 +0100

    Implement reward management module

--- rewards/migrations/0010_rewardprize_value.py
+++ rewards/migrations/0010_rewardprize_value.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rewards', '0010_rewardprize_value'),
+        ('rewards', '0009_customerreward_booking_ready_at_and_more'),
     ]
 
     operations = [
